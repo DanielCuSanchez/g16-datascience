926efdeea2c945d14729b27bf2dfb360b8047e0c
Daniel Cu <danielcu.sanchez@gmail.com>
2023-03-22 20:53:59 -0600

    Sets

---
+++ intermediate/sets.py
@@ -0,0 +1,11 @@
+# Sets: They are unordered, unchangeable, unindexed and they can not accept duplicated values. It is case sensitive.
+
+
+# mySet = {"😀", "😀", "🙂", "🤣", "🤣"}
+mySet = {'Alondra', 'Ale', 'Brenda', 'Raul','Enrique', 'Enrique', 'enrique',  'enrique'}
+
+print(type(mySet))
+
+# mySet[3] = "🥰"
+
+print(mySet)
