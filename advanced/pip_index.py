7e1a245f1f8cec0e00fe8333810ffb559db756e2
DanielCu <danielcu@alternet.com.mx>
2023-03-23 19:55:05 -0600

    pip

---
+++ advanced/pip_index.py
@@ -0,0 +1,7 @@
+# https://pypi.org/project/simple-colors/
+
+from simple_colors import *
+
+print(red('hello', ['italic', 'bold', 'underlined']))
+print(yellow('hello', 'bold'))
+print(green('hello', ['bold', 'underlined']))
